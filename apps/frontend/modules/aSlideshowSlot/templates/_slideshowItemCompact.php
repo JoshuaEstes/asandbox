472796de35cacd7782d65506c9d2a9bd3283ed6c
John Benson <johnny@punkave.com>
2010-10-21 21:19:25 +0000

    fixed the compact slideshow styles and markup
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/trunk@2473 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- apps/frontend/modules/aSlideshowSlot/templates/_slideshowItemCompact.php
+++ apps/frontend/modules/aSlideshowSlot/templates/_slideshowItemCompact.php
@@ -13,12 +13,8 @@
 <ul>
   <li class="a-slideshow-image" style="background-image:url(<?php echo $imgsrc ?>);<?php echo ($n==0)? 'display:block':'' ?>"><?php echo $embed ?></li>
   <?php if ($options['title']): ?>
-    <li class="a-slideshow-meta a-slideshow-title"><?php echo $item->title ?></li>
-  <?php endif ?>
-  <?php if ($options['description']): ?>
-    <li class="a-slideshow-meta a-slideshow-description"><div class="a-slideshow-description-wrapper"><?php echo $item->description ?></div></li>
-  <?php endif ?>
-  <?php if ($options['credit'] && $item->credit): ?>
-    <li class="a-slideshow-meta a-slideshow-credit"><?php echo __('Photo Credit: %credit%', array('%credit%' => $item->credit), 'apostrophe') ?></li>
+    <li class="a-slideshow-meta a-slideshow-title">
+			<span><?php echo $item->title ?></span>
+		</li>
   <?php endif ?>
 </ul>
