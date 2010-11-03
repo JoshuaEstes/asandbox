af43f0faabcc4eaca546d10b1be48a010099a7e2
John Benson <johnny@punkave.com>
2010-11-03 18:06:15 +0000

    - updated error500 page markup, removed error500 page references to javascript
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/trunk@2604 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- apps/frontend/config/error/error.html.php
+++ apps/frontend/config/error/error.html.php
@@ -5,21 +5,15 @@
 	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 	<meta name="title" content="Home" />
 	<title>500 Error</title>
-	<script type="text/javascript" src="/sfJqueryReloadedPlugin/js/jquery-1.3.2.min.js"></script>
-	<script type="text/javascript" src="/apostrophePlugin/js/aUI.js"></script>
-	<script type="text/javascript" src="/apostrophePlugin/js/aControls.js"></script>
-	<script type="text/javascript" src="/apostrophePlugin/js/a.js"></script>
 	<link rel="stylesheet" type="text/css" media="screen" href="/css/main.css" />
 	<link rel="shortcut icon" href="/favicon.ico" />
 </head>
 
 <body class="a-home">
-  
-<div id="a-wrapper">
-
-   <div id="a-header"><h1>500 Error.</h1></div>
-
-</div>
-
+	<div class="a-wrapper">
+		<div class="a-header">
+			<h1>500 Error.</h1>
+		</div>
+	</div>
 </body>
 </html>
