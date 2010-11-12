162d3aa0b2c9e8d89cac9955b10589cb2f1e9a52
John Benson <johnny@punkave.com>
2010-11-12 22:59:57 +0000

    change in the home template
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/trunk@2664 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- apps/frontend/modules/a/templates/homeTemplate.php
+++ apps/frontend/modules/a/templates/homeTemplate.php
@@ -113,21 +113,28 @@
 			'description' => false
 		),		
 		'aBlog' => array(
+			'maxImages' => 3, 
 			'slideshowOptions' => array(
 				'width' => 720, 
-				'height' => 320
+				'height' => 320,
+				'transition' => 'crossfade',
+				// 'position' => true, 
 			),
 		),
 		'aEvent' => array(
 			'slideshowOptions' => array(
-				'width' => 340, 
-				'height' => 220
+				'width' => 720, 
+				'height' => 220,
+				'transition' => 'crossfade',
+				// 'position' => true, 						
 			),
 		),
 		'aEventSingle' => array(
 			'slideshowOptions' => array(
-				'width' => 340, 
-				'height' => 220
+				'width' => 720, 
+				'height' => 220,
+				'transition' => 'crossfade',
+				// 'position' => true, 				
 			),
 		),
     'aText' => array(
@@ -148,7 +155,6 @@
 		'aFeed', 		
 		'aButton', 
 		'aBlog',
-		'aBlogSingle', 
 		'aEvent',
 		'aEventSingle',
 		'aText',
@@ -227,10 +233,11 @@
 		),
 		'aBlog' => array(
 			'excerptLength' => 40, 
+			'maxImages' => 3, 
 			'slideshowOptions' => array(
 				'width' => 200, 
-				'height' => 130, 
-				'resizeType' => 'c',
+				'height' => false, 
+				'resizeType' => 's',
 			),
 		),
 		'aEventSingle' => array(
