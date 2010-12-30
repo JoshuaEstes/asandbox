e6318252f6fe574739cb3304ed114419701003de
Tom Boutell <tom@punkave.com>
2010-12-30 02:04:12 +0000

    Updated examples of how to build alternate templates
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/1.5@2953 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- apps/frontend/modules/aBlog/templates/showAlternate.php
+++ apps/frontend/modules/aBlog/templates/showAlternate.php
@@ -7,17 +7,16 @@
 ?>
 <?php slot('body_class') ?>a-blog <?php echo $sf_params->get('module'); ?> <?php echo $sf_params->get('action') ?><?php end_slot() ?>
 
-<h3>This is the alternate blog post permalink page template</h3>
-
 <?php slot('a-subnav') ?>
 	<div class="a-subnav-wrapper blog a-ui clearfix">
 		<div class="a-subnav-inner">
-	    <?php include_component('aBlog', 'sidebar', array('params' => $params, 'dateRange' => $dateRange, 'categories' => $blogCategories, 'reset' => true, 'noFeed' => true)) ?>
+	    <?php include_component('aBlog', 'sidebar', array('params' => $params, 'dateRange' => $dateRange, 'info' => $info, 'reset' => true, 'noFeed' => true, 'searchLabel' => a_('Search Posts'))) ?>
 	  </div> 
 	</div>
 <?php end_slot() ?>
 
 <div id="a-blog-main" class="a-blog-main clearfix">
+  <h3>This is an example of an alternate blog template.</h3>
 	<?php echo include_partial('aBlog/post', array('a_blog_post' => $aBlogPost, 'preview' => $preview)) ?>
 
 	<?php  if (sfConfig::get('app_aBlog_disqus_enabled', true)): ?>
