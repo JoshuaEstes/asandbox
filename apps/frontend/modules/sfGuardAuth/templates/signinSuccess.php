ce98163defabe1aba90c6786ada3303a0fb8b5d1
John Benson <johnny@punkave.com>
2011-01-17 22:44:51 +0000

    1. Added Clearfix to the sfGuard success files we override in sandbox 2. Removed some CSS from main.css that is already taken care of by using the .big button class
    git-svn-id: http://svn.apostrophenow.org/sandboxes/asandbox/branches/1.5@3185 c89fab08-2f9b-42bf-8176-bd3ad6bee547

--- apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
+++ apps/frontend/modules/sfGuardAuth/templates/signinSuccess.php
@@ -4,14 +4,14 @@
 <?php slot('a-tabs', '') ?>
 <?php slot('a-login', '') ?>
 
-<div class="a-ui a-signin page" id="a-signin">
+<div class="a-ui a-signin page clearfix" id="a-signin">
   <form action="<?php echo url_for('@sf_guard_signin') ?>" method="post" id="a-signin-form" <?php echo ($form->hasErrors())? 'class="has-errors"':''; ?>>
 
 		<div class="a-form-row a-hidden">
   		<?php echo $form->renderHiddenFields() ?>
 		</div>
 		
-		<div class="a-form-row">
+		<div class="a-form-row clearfix">
     	<?php echo $form['username']->renderLabel() ?>
 			<div class="a-form-field">
     		<?php echo $form['username']->render() ?>
@@ -19,7 +19,7 @@
     	<?php echo $form['username']->renderError() ?>
 		</div>
 		
-		<div class="a-form-row">		
+		<div class="a-form-row clearfix">		
     	<?php echo $form['password']->renderLabel() ?>
 			<div class="a-form-field">
     		<?php echo $form['password']->render() ?>
@@ -27,8 +27,7 @@
     	<?php echo $form['password']->renderError() ?>
 		</div>
 		
-		<?php if (0): ?>
-		<?php // When this thing starts working, it can get displayed. ?>
+		<?php /* When this thing starts working, it can get displayed. ?>
 		<div class="a-form-row">
     	<?php echo $form['remember']->renderLabel() ?>
 			<div class="a-form-field">
@@ -36,9 +35,9 @@
 			</div>				
 			<?php echo $form['remember']->renderError() ?>
 		</div>
-		<?php endif ?>
+		<?php //*/ ?>		
 		
-		<ul class="a-form-row submit">
+		<ul class="a-form-row submit clearfix">
     	<li>
 				<input type="submit" class="a-btn big a-submit" value="<?php echo __('Sign In', null, 'apostrophe') ?>" />
 			</li>
--- apps/frontend/modules/sfGuardForgotPassword/templates/indexSuccess.php
+++ apps/frontend/modules/sfGuardForgotPassword/templates/indexSuccess.php
@@ -4,24 +4,24 @@
 <?php slot('a-tabs', '') ?>
 <?php slot('a-login', '') ?>
 
-<div class="a-ui a-signin page forgot" id="a-signin">
+<div class="a-ui a-signin page forgot clearfix" id="a-signin">
 	<form action="<?php echo url_for('@sf_guard_forgot_password') ?>" method="post">
 		
-		<div class="a-form-row">
-			<h3><?php echo __('Forgot your password?', null, 'sf_guard') ?></h3>
-		</div>
-			
 		<div class="a-form-row a-hidden">
   		<?php echo $form->renderHiddenFields() ?>
 		</div>
 		
+		<div class="a-form-row clearfix">
+			<h3><?php echo __('Forgot your password?', null, 'sf_guard') ?></h3>
+		</div>
+					
 	  <?php if ($form->hasGlobalErrors()): ?>
-		<div class="a-form-row a-errors">
+		<div class="a-form-row a-errors clearfix">
 	    <?php echo $form->renderGlobalErrors() ?>
 		</div>
 	  <?php endif; ?>
 					
-		<div class="a-form-row">
+		<div class="a-form-row clearfix">
   		<?php echo $form['email_address']->renderLabel(null, array('class' => 'a-hidden', )) ?>
 			<div class="a-form-field">
   			<?php echo $form['email_address']->render() ?>
@@ -32,7 +32,7 @@
 			</div>
 		</div>
 		
-		<div class="a-form-row submit">
+		<div class="a-form-row submit clearfix">
 			<input type="submit" class="a-btn big a-submit" name="change" value="<?php echo __('Reset Password', null, 'apostrophe') ?>" />
 		</div>
 	
